f5f49e233789e76ae635536014d93b2c85349212
daRaha <da_raha@hotmail.com>
2017-12-23 22:52:36 +0100

    qucik fix in LA

--- src/main/java/rover/Model/LogicalArea.java
+++ src/main/java/rover/Model/LogicalArea.java
@@ -24,6 +24,7 @@ public class LogicalArea extends AbstractArea {
 		abstractArea(position,width,height,areaType);
 	}
 
+	//TODO implement contains, not sure if it's needed
 	/**
 	 * 
 	 * @param point 
